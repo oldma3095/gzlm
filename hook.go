5d15607a052fb49edc9d1888bf3fd7f7919f49ba
ma3315865 <ma3315865@outlook.com>
2025-03-19 18:57:41 +0800

    on_publish oup point

--- hook.go
+++ hook.go
@@ -83,30 +83,30 @@ type OnPublishOup struct {
 	Code int    `json:"code"` // 错误代码，0代表允许推流
 	Msg  string `json:"msg"`  // 不允许推流时的错误提示
 
-	EnableHls      bool   `json:"enable_hls,omitempty"`       //	非必须，是否转换成hls-mpegts协议
-	EnableHlsFmp4  bool   `json:"enable_hls_fmp4,omitempty"`  //	非必须，是否转换成hls-fmp4协议
-	EnableMp4      bool   `json:"enable_mp4,omitempty"`       //	非必须，是否允许mp4录制
-	EnableRtsp     bool   `json:"enable_rtsp,omitempty"`      //	非必须，是否转rtsp协议
-	EnableRtmp     bool   `json:"enable_rtmp,omitempty"`      //	非必须，是否转rtmp/flv协议
-	EnableTs       bool   `json:"enable_ts,omitempty"`        //	非必须，是否转http-ts/ws-ts协议
-	EnableFmp4     bool   `json:"enable_fmp4,omitempty"`      //	非必须，是否转http-fmp4/ws-fmp4协议
-	HlsDemand      bool   `json:"hls_demand,omitempty"`       //	非必须，该协议是否有人观看才生成
-	RtspDemand     bool   `json:"rtsp_demand,omitempty"`      //	非必须，该协议是否有人观看才生成
-	RtmpDemand     bool   `json:"rtmp_demand,omitempty"`      //	非必须，该协议是否有人观看才生成
-	TsDemand       bool   `json:"ts_demand,omitempty"`        //	非必须，该协议是否有人观看才生成
-	Fmp4Demand     bool   `json:"fmp4_demand,omitempty"`      //	非必须，该协议是否有人观看才生成
-	EnableAudio    bool   `json:"enable_audio,omitempty"`     //	非必须，转协议时是否开启音频
-	AddMuteAudio   bool   `json:"add_mute_audio,omitempty"`   //	非必须，转协议时，无音频是否添加静音aac音频
-	Mp4SavePath    string `json:"mp4_save_path,omitempty"`    //	非必须，mp4录制文件保存根目录，置空使用默认
-	Mp4MaxSecond   int    `json:"mp4_max_second,omitempty"`   //	非必须，mp4录制切片大小，单位秒
-	Mp4AsPlayer    bool   `json:"mp4_as_player,omitempty"`    //	非必须，MP4录制是否当作观看者参与播放人数计数
-	HlsSavePath    string `json:"hls_save_path,omitempty"`    //	非必须，hls文件保存保存根目录，置空使用默认
-	ModifyStamp    int    `json:"modify_stamp,omitempty"`     //	非必须，该流是否开启时间戳覆盖(0:绝对时间戳/1:系统时间戳/2:相对时间戳)
-	ContinuePushMs uint32 `json:"continue_push_ms,omitempty"` //	非必须，断连续推延时，单位毫秒，置空使用配置文件默认值
-	AutoClose      bool   `json:"auto_close,omitempty"`       //	非必须，无人观看是否自动关闭流(不触发无人观看hook)
-	StreamReplace  string `json:"stream_replace,omitempty"`   //	非必须，是否修改流id, 通过此参数可以自定义流id(譬如替换ssrc)
+	EnableHls      *bool   `json:"enable_hls,omitempty"`       //	非必须，是否转换成hls-mpegts协议
+	EnableHlsFmp4  *bool   `json:"enable_hls_fmp4,omitempty"`  //	非必须，是否转换成hls-fmp4协议
+	EnableMp4      *bool   `json:"enable_mp4,omitempty"`       //	非必须，是否允许mp4录制
+	EnableRtsp     *bool   `json:"enable_rtsp,omitempty"`      //	非必须，是否转rtsp协议
+	EnableRtmp     *bool   `json:"enable_rtmp,omitempty"`      //	非必须，是否转rtmp/flv协议
+	EnableTs       *bool   `json:"enable_ts,omitempty"`        //	非必须，是否转http-ts/ws-ts协议
+	EnableFmp4     *bool   `json:"enable_fmp4,omitempty"`      //	非必须，是否转http-fmp4/ws-fmp4协议
+	HlsDemand      *bool   `json:"hls_demand,omitempty"`       //	非必须，该协议是否有人观看才生成
+	RtspDemand     *bool   `json:"rtsp_demand,omitempty"`      //	非必须，该协议是否有人观看才生成
+	RtmpDemand     *bool   `json:"rtmp_demand,omitempty"`      //	非必须，该协议是否有人观看才生成
+	TsDemand       *bool   `json:"ts_demand,omitempty"`        //	非必须，该协议是否有人观看才生成
+	Fmp4Demand     *bool   `json:"fmp4_demand,omitempty"`      //	非必须，该协议是否有人观看才生成
+	EnableAudio    *bool   `json:"enable_audio,omitempty"`     //	非必须，转协议时是否开启音频
+	AddMuteAudio   *bool   `json:"add_mute_audio,omitempty"`   //	非必须，转协议时，无音频是否添加静音aac音频
+	Mp4SavePath    *string `json:"mp4_save_path,omitempty"`    //	非必须，mp4录制文件保存根目录，置空使用默认
+	Mp4MaxSecond   *int    `json:"mp4_max_second,omitempty"`   //	非必须，mp4录制切片大小，单位秒
+	Mp4AsPlayer    *bool   `json:"mp4_as_player,omitempty"`    //	非必须，MP4录制是否当作观看者参与播放人数计数
+	HlsSavePath    *string `json:"hls_save_path,omitempty"`    //	非必须，hls文件保存保存根目录，置空使用默认
+	ModifyStamp    *int    `json:"modify_stamp,omitempty"`     //	非必须，该流是否开启时间戳覆盖(0:绝对时间戳/1:系统时间戳/2:相对时间戳)
+	ContinuePushMs *uint32 `json:"continue_push_ms,omitempty"` //	非必须，断连续推延时，单位毫秒，置空使用配置文件默认值
+	AutoClose      *bool   `json:"auto_close,omitempty"`       //	非必须，无人观看是否自动关闭流(不触发无人观看hook)
+	StreamReplace  *string `json:"stream_replace,omitempty"`   //	非必须，是否修改流id, 通过此参数可以自定义流id(譬如替换ssrc)
 
-	AudioTranscode bool `json:"audio_transcode,omitempty"` // pro版本，自动opus->aac
+	AudioTranscode *bool `json:"audio_transcode,omitempty"` // pro版本，自动opus->aac
 }
 
 type OnRecordMp4Inp struct {
